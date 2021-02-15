ef2c01c3ab87c41f5e9b16d1587db374c394136c
jaredrichardson <jrichardson@mail.sfsu.edu>
2021-02-14 23:36:48 -0800

    new

--- src/main/java/edu/csc413/collections/Classroom.java
+++ src/main/java/edu/csc413/collections/Classroom.java
@@ -8,38 +8,89 @@ public class Classroom {
     private static final int WAIT_LIST_CAPACITY = 5;
 
     // Instance variables.
-    private HashMap<Integer, Student> registeredStudents;
-    private HashSet<Integer> enrolledIds;
-    private Queue<Integer> waitlistIds;
+    private final HashMap<Integer, Student> registeredStudents;
+    private final HashSet<Integer> enrolledIds;
+    private final Queue<Integer> waitlistIds;
 
     public Classroom() {
         // TODO: Implement. Initialize any instance variables here.
+        registeredStudents = new HashMap<>();
+        enrolledIds = new HashSet<>();
+        waitlistIds = new ArrayDeque<>();
+
     }
 
     public void registerStudent(Student student) {
         // TODO: Implement. The student should be registered, but not enrolled in the class or added to the waitlist.
+        registeredStudents.put(student.getId(), student);
     }
 
     public void enrollStudent(int id) {
-        // TODO: Implement. The student with the provided ID should be added the enrolled students set if there is
-        //       capacity. If there is not, but there is capacity in the waitlist, the student should be added to that
-        //       instead. If there is no capacity in the enrollment set or the waitlist, the request can be ignored.
-    }
+        Student student = registeredStudents.get(id);
+        while (student != null) {
+            break;
+        }
+           if (this.enrolledIds.size() < CLASS_CAPACITY) {
+                this.enrolledIds.add(id);
+            }
+            else if(this.waitlistIds.size() < WAIT_LIST_CAPACITY) {
+                this.waitlistIds.add(id);
+            }
+
+            }
+
+
+    // TODO: Implement. The student with the provided ID should be added the enrolled students set if there is
+    //       capacity. If there is not, but there is capacity in the waitlist, the student should be added to that
+    //       instead. If there is no capacity in the enrollment set or the waitlist, the request can be ignored.
+
 
     public void dropStudent(int id) {
         // TODO: Implement. Attempt to remove the student with the provided ID from the enrolled students set. If the
-        //       student was removed, backfill the enrolled students set with a student from the waitlist.
+        //       student was removed, backfill the enrolled students set with a student from the waitlist
+        this.enrolledIds.remove(id);
+        if (this.waitlistIds.size() > 0) {
+            int waitlistedid = waitlistIds.remove();
+
+
+        while (this.registeredStudents.containsKey(waitlistedid)) {
+            break;
+        }
+            this.enrolledIds.add(waitlistedid);
+
+        }
     }
+
+
+
 
     public ArrayList<String> getEnrolledStudents() {
         // TODO: Implement. Return the names of all students that are enrolled in an ArrayList<Student>.
-        return new ArrayList<>();
+        ArrayList<String> nameEnrollmentList = new ArrayList<>();
+        for (int id : this.enrolledIds) {
+            Student student = this.registeredStudents.get(id);
+            while (student != null) {
+                break;
+            }
+            nameEnrollmentList.add(Objects.requireNonNull(student).getName());
+        }
+        return nameEnrollmentList;
     }
 
     public ArrayList<String> getWaitlistedStudents() {
         // TODO: Implement. Return the names of all of the students that are in the waitlist in an ArrayList<Student>.
         //       They should be in the same order that they are in the waitlist.
-        return new ArrayList<>();
+        ArrayList<String> waitlistNames = new ArrayList<>();
+        for (int id : this.waitlistIds) {
+            Student student = this.registeredStudents.get(id);
+            while (student != null) {
+                break;
+
+            }
+            assert student != null;
+            waitlistNames.add(student.getName());
+        }
+        return waitlistNames;
     }
 
     public static void main(String[] args) {
@@ -54,8 +105,8 @@ public class Classroom {
         }
 
         // Attempt to enroll all students. This will go in alphabetical order by student name.
-        for (int i = 0; i < IDS.length; i++) {
-            classroom.enrollStudent(IDS[i]);
+        for (int id : IDS) {
+            classroom.enrollStudent(id);
         }
 
         // Attempt to drop a few students from the class, and re-enroll one.
@@ -66,14 +117,14 @@ public class Classroom {
 
         // Print out all enrolled students.
         System.out.println("Enrolled students:");
-        for (String studentName: classroom.getEnrolledStudents()) {
+        for (String studentName : classroom.getEnrolledStudents()) {
             System.out.println(studentName);
         }
         System.out.println();
 
         // Print out all enrolled students.
         System.out.println("Waitlist:");
-        for (String studentName: classroom.getWaitlistedStudents()) {
+        for (String studentName : classroom.getWaitlistedStudents()) {
             System.out.println(studentName);
         }
         System.out.println();
